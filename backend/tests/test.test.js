c2674743236df49623e444831322f9d3f93ce7be
Samuel Porto <samuelpporto@gmail.com>
2022-09-02 21:04:40 -0300

    Adicionado scripts de teste

---
+++ backend/tests/test.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("Teste de exemplo", () => {
+    it("Deve retornar 200", async () => {
+        const response = await request(app).get("/");
+        expect(response.statusCode).toBe(200);
+    });
+    
+});
