a0c600143e7e11bc267febb3f17bd4b1907a5d9c
Samuel Porto <samuelpporto@gmail.com>
2022-09-18 00:59:23 -0300

    Adicionada funcionalidade Login #2

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,6 +1,39 @@
 import { Link } from "react-router-dom";
+import Axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { changeUser } from "../../redux/userSlice";
 
 export function Login() {
+  const dispatch = useDispatch();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    return Axios.post(`${process.env.REACT_APP_API_URL}/auth`, {
+      email: user.email,
+      password: user.password,
+    }).then((response) => {
+      setLoading(false);
+      if (response.status === 200 && response.statusText === "OK") {
+        dispatch(
+          changeUser({
+            name: response.data.user.firstName,
+            id: response.data.user._id,
+          })
+        );
+        localStorage.setItem("authorization", `Bearer ${response.data.token}`);
+        return true;
+      }
+    });
+  };
+
   return (
     <div className="flex max-w-6xl m-auto justify-center ">
       <div className="flex min-h-screen items-center ">
@@ -8,17 +41,23 @@ export function Login() {
           <h2 className="mt-6 text-center text-2xl font-bold text-gray-900">
             Entrar com sua conta
           </h2>
-          <form>
+          <form onSubmit={handleLogin}>
             <div className="mt-4">
               <input
                 type="email"
                 className="appearance-none rounded-none w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email"
+                onChange={(event) =>
+                  setUser({ ...user, email: event.target.value })
+                }
               />
               <input
                 type="password"
                 className="mt-3 appearance-none rounded-none w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Senha"
+                onChange={(event) =>
+                  setUser({ ...user, password: event.target.value })
+                }
               />
             </div>
 
@@ -28,9 +67,7 @@ export function Login() {
                   type="checkbox"
                   className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
                 />
-                <label
-                  className="ml-2 block text-sm text-gray-900"
-                >
+                <label className="ml-2 block text-sm text-gray-900">
                   Lembrar-me
                 </label>
               </div>
@@ -44,9 +81,7 @@ export function Login() {
                 </Link>
               </div>
             </div>
-            <button
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
+            <button className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               Entrar
             </button>
           </form>
