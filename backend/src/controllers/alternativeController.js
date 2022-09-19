c6f1a240ef5a5b72d514caa7c0d8227c27a61805
cenfv <59135587+cenfv@users.noreply.github.com>
2022-09-19 12:23:28 -0300

    Model, Controller e rota das alternativas #3

---
+++ backend/src/controllers/alternativeController.js
@@ -0,0 +1,36 @@
+const Alternative = require("../models/Alternative");
+
+const handleErrors = (err) => {
+  let errors = {};
+
+  Object.values(err.errors).forEach(({ properties }) => {
+    errors[properties.path] = properties.message;
+  });
+
+  return errors;
+};
+
+exports.getAllAlternatives = async () => {
+  const alternative = await Alternative.find();
+  if (alternative) {
+    return alternative;
+  }
+};
+
+exports.getAlternativeById = async (id) => {
+  const alternative = await Alternative.findById(id);
+  if (alternative) {
+    return alternative;
+  }
+};
+
+exports.createAlternative = async (alternative) => {
+  try {
+    const res = Alternative.insertMany(alternative);
+    return res;
+  } catch (err) {
+    console.log(err);
+    const errors = handleErrors(err);
+    throw errors;
+  }
+};
---
+++ backend/src/routes/questions/alternative.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const alternativeController = require("../../controllers/alternativeController");
+const checkToken = require("../../helpers/checkToken");
+
+router.get("/", checkToken.checkTokenBearer, async (req, res, next) => {
+  try {
+    const alternatives = await alternativeController.getAllAlternatives();
+    return res.status(200).json({
+      alternatives,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({
+      msg: "Alternative not found",
+    });
+  }
+});
+
+router.get("/:id", checkToken.checkTokenBearer, async (req, res, next) => {
+  const alternativeId = req.params.id;
+  try {
+    const alternative = await alternativeController.getAlternativeById(
+      alternativeId
+    );
+    return res.status(200).json({
+      alternative,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({
+      msg: "Alternative not found",
+    });
+  }
+});
+
+router.post("/", checkToken.checkTokenBearer, async (req, res, next) => {
+  const alternatives = req.body.alternatives;
+  try {
+    const alternative = await alternativeController.createAlternative(
+      alternatives
+    );
+    return res.status(201).json({
+      alternative,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      validationError: err,
+    });
+  }
+});
+
+module.exports = router;
