180cc083454b9008e2d8183fc4c8eff022c15012
hebertwilly <77469709+hebertwilly@users.noreply.github.com>
2022-09-22 00:52:30 -0300

    atualizado navbar para usuario logado #2

--- frontend/src/components/LoggedNavbar/index.js
+++ frontend/src/components/LoggedNavbar/index.js
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser, logout } from "../../redux/userSlice";
 
 export function LoggedNavbar() {
+
+  const { name, isLogged } = useSelector(selectUser);
+  const dispatch = useDispatch();
+
   return (
     <div className="border-b-2 bg-white  border-gray-100 shadow-sm">
       <div className="py-9 items-center justify-center max-w-6xl mx-auto ">
@@ -22,8 +28,17 @@ export function LoggedNavbar() {
 
           <div className="flex space-x-10 items-center">
             <span className="text-base font-medium text-gray-500">
-              Seja bem-vindo, Usuario
+              Seja bem-vindo, {name}
             </span>
+            <Link
+              to="/home"
+              onClick={() => {
+                localStorage.removeItem("authorization");
+                dispatch(logout());
+              }}
+            >
+              <p className="text-base font-medium text-red-600">Sair</p>
+            </Link>
           </div>
         </div>
       </div>
