cd1fb959e0930bb95eae9966869ea782ca64c7db
Samuel Porto <samuelpporto@gmail.com>
2022-09-23 14:42:11 -0300

    Adicionados testes para questionAlternative #3

--- backend/tests/questionAlternative.test.js
+++ backend/tests/questionAlternative.test.js
@@ -3,6 +3,8 @@ const app = require("../app");
 const MongoMemoryServer = require("../src/database/mongodb/MongoMemoryServer");
 
 let token = "";
+let questionId = "";
+let alternativeId = "";
 
 beforeAll(async () => {
   await MongoMemoryServer.connect();
@@ -18,38 +20,38 @@ beforeAll(async () => {
     password: "werty2510",
   });
   token = response.body.token;
+
+  const questionResponse = await request(app)
+    .post("/question")
+    .set("Authorization", `Bearer ${token}`)
+    .send({
+      title: "Questão numero 1",
+      description: "questao das bananas",
+      difficulty: 0,
+      editionYear: 2017,
+    });
+  questionId = questionResponse.body.question._id;
+
+  const alternativeResponse = await request(app)
+    .post("/alternative")
+    .set("Authorization", `bearer ${token}`)
+    .send({
+      alternatives: [
+        { description: "Alternativa 1" },
+        { description: "Alternativa 2" },
+        { description: "Alternativa 3" },
+        { description: "Alternativa 4" },
+      ],
+    });
+  alternativeId = alternativeResponse.body.alternative.map(
+    (alternative) => alternative._id
+  );
 });
 
 afterAll(async () => await MongoMemoryServer.closeDatabase());
 
 describe("Create question-alternative", () => {
   it("Should be able to create a new question-alternative", async () => {
-    const questionResponse = await request(app)
-      .post("/question")
-      .set("Authorization", `Bearer ${token}`)
-      .send({
-        title: "Questão numero 1",
-        description: "questao das bananas",
-        difficulty: 0,
-        editionYear: 2017,
-      });
-    let questionId = questionResponse.body.question._id;
-
-    const alternativeResponse = await request(app)
-      .post("/alternative")
-      .set("Authorization", `bearer ${token}`)
-      .send({
-        alternatives: [
-          { description: "Alternativa 1" },
-          { description: "Alternativa 2" },
-          { description: "Alternativa 3" },
-          { description: "Alternativa 4" },
-        ],
-      });
-
-    let alternativeId = alternativeResponse.body.alternative.map(
-      (alternative) => alternative._id
-    );
     const response = await request(app)
       .post("/question-alternative")
       .set("Authorization", `bearer ${token}`)
@@ -59,5 +61,81 @@ describe("Create question-alternative", () => {
         correctAlternative: alternativeId.at(0),
       });
     expect(response.status).toBe(201);
+  });
+
+  describe("Create question-alternative with empty Fields", () => {
+    it("Should not be able to create a new question-alternative with empty question", async () => {
+      const response = await request(app)
+        .post("/question-alternative")
+        .set("Authorization", `bearer ${token}`)
+        .send({
+          question: "",
+          alternative: alternativeId,
+          correctAlternative: alternativeId.at(0),
+        });
+      expect(response.status).toBe(400);
+    });
+
+    it("Should not be able to create a new question-alternative with empty alternative", async () => {
+      const response = await request(app)
+        .post("/question-alternative")
+        .set("Authorization", `bearer ${token}`)
+        .send({
+          question: questionId,
+          alternative: "",
+          correctAlternative: alternativeId.at(0),
+        });
+      expect(response.status).toBe(400);
+    });
+
+    it("Should not be able to create a new question-alternative with empty correctAlternative", async () => {
+      const response = await request(app)
+        .post("/question-alternative")
+        .set("Authorization", `bearer ${token}`)
+        .send({
+          question: questionId,
+          alternative: alternativeId,
+          correctAlternative: "",
+        });
+      expect(response.status).toBe(400);
+    });
+  });
+
+  describe("Create question-alternative with invalid Fields", () => {
+    it("Should not be able to create a new question-alternative with invalid question", async () => {
+      const response = await request(app)
+        .post("/question-alternative")
+        .set("Authorization", `bearer ${token}`)
+        .send({
+          question: "123",
+          alternative: alternativeId,
+          correctAlternative: alternativeId.at(0),
+        });
+      expect(response.status).toBe(400);
+    });
+
+    it("Should not be able to create a new question-alternative with invalid alternative", async () => {
+      const response = await request(app)
+        .post("/question-alternative")
+        .set("Authorization", `bearer ${token}`)
+        .send({
+          question: questionId,
+          alternative: "123",
+          correctAlternative: alternativeId.at(0),
+        });
+      expect(response.status).toBe(400);
+    });
+
+    it("Should not be able to create a new question-alternative with invalid correctAlternative", async () => {
+      const response = await request(app)
+        .post("/question-alternative")
+        .set("Authorization", `bearer ${token}`)
+        .send({
+          question: questionId,
+          alternative: alternativeId,
+          correctAlternative: "123",
+        });
+      expect(response.status).toBe(400);
+    });
   });
 });
