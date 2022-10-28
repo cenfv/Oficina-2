db39c58002e530d4f766b557e9caf78ecc8b3e6c
Samuel Porto <samuelpporto@gmail.com>
2022-10-28 20:43:58 -0300

    Adicionada funcionalidade criar provas #6

--- frontend/src/pages/AddQuiz/index.js
+++ frontend/src/pages/AddQuiz/index.js
@@ -1,7 +1,64 @@
 import { Footer } from "../../components/Footer";
 import { LoggedNavbar } from "../../components/LoggedNavbar";
+import { useState } from "react";
+import Axios from "axios";
 
 export function AddQuiz() {
+  const [quizContent, setQuizContent] = useState({
+    description: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const [status, setStatus] = useState(false);
+
+  const handleCreateQuiz = async () => {
+    setLoading(true);
+    return Axios.post(
+      `${process.env.REACT_APP_API_URL}/quiz`,
+      {
+        description: quizContent.description,
+      },
+      {
+        headers: {
+          authorization: localStorage.getItem("authorization"),
+        },
+      }
+    )
+      .then((response) => {
+        setLoading(false);
+        if (response.status === 201 && response.statusText === "Created") {
+          return response.data.quiz;
+        }
+        return null;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const addQuiz = async (e) => {
+    e.preventDefault();
+    let saveDataForm;
+    try {
+      saveDataForm = await handleCreateQuiz();
+    } catch (err) {
+      setLoading(false);
+      if (
+        err.response.status === 400 &&
+        err.response.statusText === "Bad Request"
+      ) {
+        setStatus({
+          type: "error",
+          message: "Houve um erro ao criar o quiz!",
+        });
+      }
+    }
+    setStatus({
+      type: "success",
+      message: "Quiz cadastrado com sucesso!",
+    });
+  };
 
   return (
     <>
@@ -19,11 +76,14 @@ export function AddQuiz() {
             <h3 className="mt-5 text-2xl font-bold text-gray-900">
               Criar nova prova:
             </h3>
-            <form>
+            <form onSubmit={addQuiz}>
               <div className="grid grid-cols-1">
                 <input
                   className="bg-white rounded-lg p-4 drop-shadow-lg mt-3 focus:outline-none focus:ring"
                   placeholder="Nome da prova"
+                  onChange={(e) =>
+                    setQuizContent({ description: e.target.value })
+                  }
                 ></input>
               </div>
               <div className="flex justify-end">
