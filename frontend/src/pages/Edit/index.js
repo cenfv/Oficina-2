531dce89a28910df28e250ffbdf62e29a8b6e4d3
Samuel Porto <samuelpporto@gmail.com>
2022-11-02 18:36:57 -0300

    Adicionada DataTable na página Editar #5

--- frontend/src/pages/Edit/index.js
+++ frontend/src/pages/Edit/index.js
@@ -1,7 +1,34 @@
 import { LoggedNavbar } from "../../components/LoggedNavbar";
 import { Footer } from "../../components/Footer";
+import { DataTable } from "../../components/DataTable";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
 export function Edit() {
+  const [questionAlternativesData, setQuestionAlternativesData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const handleLoadQuestionAlternativesData = async () => {
+    setLoading(true);
+    Axios.get(`${process.env.REACT_APP_API_URL}/question-alternative`, {
+      headers: {
+        authorization: localStorage.getItem("authorization"),
+      },
+    })
+      .then((response) => {
+        setLoading(false);
+        if (response.status === 200 && response.statusText === "OK") {
+          setQuestionAlternativesData(response.data.questionAlternative);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    handleLoadQuestionAlternativesData();
+  }, []);
+
   return (
     <>
       <div className="min-h-screen bg-gray-50">
@@ -14,6 +41,7 @@ export function Edit() {
             Edite ou remova o conteúdo das suas questões.
           </p>
           <h3 className="mt-5 text-2xl font-bold text-gray-900">Questões:</h3>
+          <DataTable data={questionAlternativesData} setData={setQuestionAlternativesData}/>
         </div>
       </div>
       <Footer />
