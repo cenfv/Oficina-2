482d22d2d014f8c3c7e88a42365261db1cdd0011
cenfv <59135587+cenfv@users.noreply.github.com>
2022-11-21 11:58:52 -0300

    Verifica se a resposta inserida é a correta #9  #10

--- backend/src/controllers/submissionController.js
+++ backend/src/controllers/submissionController.js
@@ -1,52 +1,70 @@
 const Submission = require("../models/Submission");
+const Alternative = require("../models/Alternative");
+const QuestionAlternative = require("../models/QuestionAlternative");
 
 const handleErrors = (err) => {
-    let errors = {};
+  let errors = {};
 
-    Object.values(err.errors).forEach(({ properties }) => {
-        errors[properties.path] = properties.message;
-    });
+  Object.values(err.errors).forEach(({ properties }) => {
+    errors[properties.path] = properties.message;
+  });
 
-    return errors;
+  return errors;
 };
 
 exports.getAllSubmission = async () => {
-    const submission = await Submission.find();
-    if (submission) {
-        return submission;
-    }
+  const submission = await Submission.find();
+  if (submission) {
+    return submission;
+  }
 };
 exports.getSubmissionById = async (id) => {
-    const submission = await Submission.findById(id);
-    if (submission) {
-        return submission;
-    }
+  const submission = await Submission.findById(id);
+  if (submission) {
+    return submission;
+  }
 };
 
 exports.deleteSubmission = async (id) => {
-    try {
-      const submission = await Submission.findByIdAndDelete(id);
-      if (submission) {
-        return submission;
-      }
-    } catch (err) {
-      console.log(err);
-      const errors = handleErrors(err);
-      throw errors;
+  try {
+    const submission = await Submission.findByIdAndDelete(id);
+    if (submission) {
+      return submission;
     }
-  };
-
-  exports.createSubmission = async (userId, questionAlternativeId, choiceId) => {
-    try {
-      const submission = new Submission({
-        user: userId,
-        questionAlternative: questionAlternativeId,
-        choice: choiceId,
-      });
-      const res = await submission.save();
-      return res;
-    } catch (err) {
-      const errors = handleErrors(err);
-      throw errors;
-    }
+  } catch (err) {
+    console.log(err);
+    const errors = handleErrors(err);
+    throw errors;
   }
+};
+
+exports.createSubmission = async (userId, questionAlternativeId, choiceId) => {
+  try {
+    const questionAlternative = await QuestionAlternative.findById(
+      questionAlternativeId
+    );
+    const choice = await Alternative.findById(choiceId);
+    const corretAlternative = await Alternative.findById(
+      questionAlternative.correctAlternative
+    );
+
+    let isCorrect = false;
+    if (JSON.stringify(choice) === JSON.stringify(corretAlternative)) {
+      isCorrect = true;
+    }
+
+    const submission = new Submission({
+      user: userId,
+      questionAlternative: questionAlternativeId,
+      choice: choiceId,
+      correctChoice: isCorrect,
+    });
+
+    const res = await submission.save();
+    return res;
+  } catch (err) {
+    console.log(err)
+    const errors = handleErrors(err);
+    throw errors;
+  }
+}
